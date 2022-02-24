26f09abd8bf7bc85b404f1bb307f8250e7157079
gy011694 <100341209+gy011694@users.noreply.github.com>
2022-02-24 15:40:04 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 50) return '顺顺说别气馁';
+        if (cps <= 100) return '顺顺说再试一次';
+        if (cps <= 125)  return '再吃顺顺就没啦';
+        if (cps <= 150) return '妈妈棒！';
         return '人？';
     }
 
